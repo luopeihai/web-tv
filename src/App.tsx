c5ff9ef54ad8b9faa963529a759dde169077cb6e
Peihai Luo （罗培海） <phluo@trip.com>
2023-09-03 17:20:40 +0800

    chore: build

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,8 @@ function App() {
         <Router>
           <Header />
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route path="/" component={Home} />
+            <Route path="/home" component={Home} />
             <Route path="/shows/:id" component={Detail} />
           </Switch>
         </Router>
